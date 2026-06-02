7ebcd44af86bc8e4d7b2d78c86aeefc44501d80d
Adrien Deval <adrien.deval@proton.me>
2026-06-02 16:54:55 +0200

    Add configuration for CSV and TMDB URLs

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTytB0wIpk56Pkxt8KDgSva7USl1cCm5ZBrbuaKGta9740RSrrJluXZLQ3x5J6OxWmKdxBhJH9bMsOD/pub?output=csv",
+  TMDB_BASE_URL: "https://www.themoviedb.org/movie/"
+};
